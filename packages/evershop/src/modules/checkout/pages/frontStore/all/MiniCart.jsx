b30ae314a8ef98a89bc136a35840619597313980
Vlad <shirker2006@gmail.com>
2024-10-13 16:26:00 -0700

    up

--- packages/evershop/src/modules/checkout/pages/frontStore/all/MiniCart.jsx
+++ packages/evershop/src/modules/checkout/pages/frontStore/all/MiniCart.jsx
@@ -1,44 +1,30 @@
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import React from 'react';
 import './MiniCart.scss';
-import Bag from '@heroicons/react/outline/ShoppingBagIcon';
-import { useAppState } from '@components/common/context/app';
-import { get } from '@evershop/evershop/src/lib/util/get';
+// import Bag from '@heroicons/react/outline/ShoppingBagIcon';
+//import { useAppState } from '@components/common/context/app';
+//import { get } from '@evershop/evershop/src/lib/util/get';
 
-export default function MiniCart({ cartUrl, cart }) {
-  const miniCart = get(useAppState(), 'cart', cart || {});
+export default function MiniCart() {
+  // const miniCart = get(useAppState(), 'cart', cart || {});
 
   return (
-    <div className="mini-cart-wrapper self-center">
+    <span />
+  );
+}
+
+/*
+{false &&<div className="mini-cart-wrapper self-center">
       <a className="mini-cart-icon" href={cartUrl}>
         <Bag width={20} height={20} />
         {miniCart.totalQty > 0 && <span>{miniCart.totalQty}</span>}
       </a>
-    </div>
-  );
-}
+    </div>}
+    */
 
-MiniCart.propTypes = {
-  cartUrl: PropTypes.string.isRequired,
-  cart: PropTypes.shape({
-    totalQty: PropTypes.number
-  })
-};
-
-MiniCart.defaultProps = {
-  cart: null
-};
 
 export const layout = {
   areaId: 'icon-wrapper',
   sortOrder: 10
 };
 
-export const query = `
-  query Query {
-    cartUrl: url(routeId: "cart"),
-    cart(id: getContextValue("cartId", null)) {
-      totalQty
-    }
-  }
-`;
