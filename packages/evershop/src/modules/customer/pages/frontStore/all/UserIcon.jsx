cf7842830537c442e8f3e48ab6daffe51accfd27
Vlad <shirker2006@gmail.com>
2024-10-13 18:17:19 -0700

    up

--- packages/evershop/src/modules/customer/pages/frontStore/all/UserIcon.jsx
+++ packages/evershop/src/modules/customer/pages/frontStore/all/UserIcon.jsx
@@ -1,45 +1,17 @@
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import React from 'react';
-import Icon from '@heroicons/react/outline/UserIcon';
+//import Icon from '@heroicons/react/outline/UserIcon';
 
-export default function UserIcon({ customer, accountUrl, loginUrl }) {
+export default function UserIcon() {
   return (
-    <div className="self-center">
-      <a href={customer ? accountUrl : loginUrl}>
-        <Icon width={25} height={25} />
-      </a>
-    </div>
+<span></span>
   );
 }
 
-UserIcon.propTypes = {
-  accountUrl: PropTypes.string,
-  customer: PropTypes.shape({
-    email: PropTypes.string.isRequired,
-    fullName: PropTypes.string.isRequired,
-    uuid: PropTypes.string.isRequired
-  }),
-  loginUrl: PropTypes.string.isRequired
-};
 
-UserIcon.defaultProps = {
-  accountUrl: null,
-  customer: null
-};
 
 export const layout = {
   areaId: 'icon-wrapper',
   sortOrder: 30
 };
 
-export const query = `
-  query Query {
-    customer: currentCustomer {
-      uuid
-      fullName
-      email
-    }
-    accountUrl: url(routeId: "account")
-    loginUrl: url(routeId: "login")
-  }
-`;
